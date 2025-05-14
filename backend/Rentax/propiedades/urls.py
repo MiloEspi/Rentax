38dde55895a50845b88aca20ce11b3c8753ba668
MiloEspi <espinazocamilo@gmail.com>
2025-05-14 18:34:29 -0300

    agregue las importaciones de las vistas de propie dades

--- backend/Rentax/propiedades/urls.py
+++ backend/Rentax/propiedades/urls.py
@@ -1,3 +1,17 @@
+from django.urls import path
+
+from .views import (
+    PropiedadListCreateView,
+    PoliticaListView,
+    PoliticaSinReembolsoCreateView,
+    PoliticaConReembolsoCompletoCreateView,
+    PoliticaConReembolsoParcialCreateView,
+    LocalidadListView,
+    ViviendaCreateView,
+    CocheraCreateView,
+    LocalCreateView,
+    PropiedadDetailView,
+)
 
 urlpatterns = [
     path('propiedades/', PropiedadListCreateView.as_view(), name='propiedad-list-create'),
@@ -7,7 +21,7 @@ urlpatterns = [
     path('politicas/con_reembolso_parcial/', PoliticaConReembolsoParcialCreateView.as_view(), name='politica-con-reembolso-parcial-create'),
     path('localidades/', LocalidadListView.as_view(), name='localidad-list'),
     path('viviendas/', ViviendaCreateView.as_view(), name='vivienda-create'),
-    path('cocheras/',CocheraCreateView.as_view(),name='cochera-create'),
+    path('cocheras/', CocheraCreateView.as_view(), name='cochera-create'),
     path('local/', LocalCreateView.as_view(), name='local-create'),
-     path ('propiedades/<int:pk>/', PropiedadDetailView.as_view(), name='propiedad-detail'),
+    path('propiedades/<int:pk>/', PropiedadDetailView.as_view(), name='propiedad-detail'),
 ]
